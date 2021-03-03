7430b33fe1ecbf7d8f7cd7e463a0eff1e1e8c430
Ruslan Popov <ruslanchik.popov@gmail.com>
2021-03-03 12:48:18 +0700

    Implemented distribution of the matrix B

--- lab3/main.c
+++ lab3/main.c
@@ -20,6 +20,33 @@ double* initMatrix(const int X_dim, const int Y_dim){
     return M;
 }
 
+
+double *dataDistributionB(double* B, int *dataForEachProc, int *shiftForEachProc, int numProcs, int rank){
+    MPI_Datatype allCol, coltype;
+
+    MPI_Type_vector(n2, 1, n3, MPI_DOUBLE, &allCol);
+    MPI_Type_commit(&allCol);
+    MPI_Type_create_resized(allCol, 0, sizeof(double), &coltype);
+    MPI_Type_commit(&coltype);
+
+    dataForEachProc[0] = (n3 / numProcs);
+    shiftForEachProc[0] = 0;
+    int restCols = n3;
+    for (int i = 1; i < numProcs; ++i){
+        restCols -= dataForEachProc[i-1];
+        dataForEachProc[i] = restCols  / (numProcs - i);
+        shiftForEachProc[i] = shiftForEachProc[i-1] + dataForEachProc[i-1];
+    }
+
+    double *blockData = (double*)malloc(sizeof(double) * dataForEachProc[rank] * n2);
+
+    MPI_Scatterv(B, dataForEachProc, shiftForEachProc, coltype, blockData, dataForEachProc[rank] * n2,
+                 MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    return blockData;
+}
+
+
 double* dataDistributionA(double *A, int *dataForEachProc, int *shiftForEachProc, int numProcs, int rank){
     dataForEachProc[0] = (n1 / numProcs) * n2;
     shiftForEachProc[0] = 0;
@@ -34,6 +61,7 @@ double* dataDistributionA(double *A, int *dataForEachProc, int *shiftForEachProc
                  MPI_DOUBLE, 0, MPI_COMM_WORLD);
     return blockData;
 }
+
 
 int main(int argc, char **argv) {
     double *A, *B, *C;
@@ -56,15 +84,16 @@ int main(int argc, char **argv) {
     }
 
     blockDataA = dataDistributionA(A, dataForEachProc, shiftForEachProc, size, rank);
+    blockDataB = dataDistributionB(B, dataForEachProc, shiftForEachProc, size, rank);
 
-    if (rank == 2){
+   /* if (rank == 0){
         putchar('\n');
-        for (int i = 0; i < dataForEachProc[rank] / n2; ++i){
+        for (int i = 0; i < dataForEachProc[rank]; ++i){
             for (int j = 0; j < n2; ++j){
-                printf("%lf ", blockDataA[i*n2 + j]);
+                printf("%lf ", blockDataB[i*n2 + j]);
             } putchar('\n');
         }
-    }
+    }*/
 
     MPI_Finalize();
     if (rank == 0){
@@ -74,6 +103,7 @@ int main(int argc, char **argv) {
     }
 
     free(blockDataA);
+    free(blockDataB);
     free(dataForEachProc);
     free(shiftForEachProc);
     return 0;
