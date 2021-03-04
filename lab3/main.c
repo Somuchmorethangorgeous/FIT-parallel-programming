b0f7f8ebea44bf5feded7ac470d6f32d3e604561
Ruslan Popov <ruslanchik.popov@gmail.com>
2021-03-04 21:59:45 +0700

    Deleted all previous code. Implemented distribution of matrices to nodes.

--- lab3/main.c
+++ lab3/main.c
@@ -1,141 +1,118 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <malloc.h>
 #include <mpi.h>
 
-const int n1 = 5;
+
+const int n1 = 4;
 const int n2 = 8;
-const int n3 = 5;
+const int n3 = 4;
 
 
 double* initMatrix(const int X_dim, const int Y_dim){
     double *M = (double*)malloc(sizeof(double) * X_dim * Y_dim);
     for (int i = 0; i < X_dim; ++i){
         for (int j = 0; j < Y_dim; ++j){
-            M[i*Y_dim + j] = rand()%10;
+            M[i*Y_dim + j] = rand() % 10;
         }
     }
     return M;
 }
 
 
-double* calculateMatrixC(const int X_dim, const int Y_dim, const double *blockDataA, const double *blockDataB){
-    double *blockDataC = (double*)malloc(sizeof(double) * X_dim * Y_dim);
-    for (int i = 0; i < X_dim; ++i){
-        for (int j = 0; j < Y_dim; ++j){
-            blockDataC[i*Y_dim + j] = 0.0;
-            for (int k = 0; k < n2; ++k){
-                blockDataC[i*Y_dim + j] += blockDataA[i*n2 + k] * blockDataB[j*n2 + k];
-            }
-        }
+void createCommunicators(int* coords, int *dims, const int size, MPI_Comm *colCom, MPI_Comm *rowCom){
+    int periods[2] = {0, 0};
+    MPI_Comm comm2d;
+    int rank;
+
+    MPI_Dims_create(size, 2, dims);
+    MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, 0, &comm2d);
+
+    MPI_Comm_rank(comm2d, &rank);
+    MPI_Cart_get(comm2d, 2, dims, periods, coords);
+
+    MPI_Comm_split(comm2d, coords[1], coords[0], rowCom);
+    MPI_Comm_split(comm2d, coords[0], coords[1], colCom);
+}
+
+void dataDistributionOnNodes(const double *A, const double *B, double *blockDataA, double *blockDataB, MPI_Comm rowCom, MPI_Comm colCom, const int *coords, const int sizeX, const int sizeY){
+    if (coords[1] == 0) {
+        MPI_Scatter(A, n1 * n2 / sizeX, MPI_DOUBLE, blockDataA, n1 * n2 / sizeX, MPI_DOUBLE, 0,
+                    rowCom);
     }
-    return blockDataC;
+    if (coords[0] == 0) {
+        MPI_Datatype allCol, coltype;
+        MPI_Type_vector(n2, 1, n3, MPI_DOUBLE, &allCol);
+        MPI_Type_commit(&allCol);
+        MPI_Type_create_resized(allCol, 0, sizeof(double), &coltype);
+        MPI_Type_commit(&coltype);
+        MPI_Scatter(B, n3 / sizeY, coltype, blockDataB, n2 * n3 / sizeY, MPI_DOUBLE, 0,
+                    colCom);
+    }
+
+    MPI_Bcast(blockDataA, n1*n2 / sizeY, MPI_DOUBLE, 0, colCom);
+    MPI_Bcast(blockDataB, n2*n3 / sizeX, MPI_DOUBLE, 0, rowCom);
 }
 
 
-double *dataDistributionB(double* B, int *dataForEachProc, int *shiftForEachProc, int numProcs, int rank){
-    MPI_Datatype allCol, coltype;
-
-    MPI_Type_vector(n2, 1, n3, MPI_DOUBLE, &allCol);
-    MPI_Type_commit(&allCol);
-    MPI_Type_create_resized(allCol, 0, sizeof(double), &coltype);
-    MPI_Type_commit(&coltype);
-
-    dataForEachProc[0] = (n3 / numProcs);
-    shiftForEachProc[0] = 0;
-    int restCols = n3;
-    for (int i = 1; i < numProcs; ++i){
-        restCols -= dataForEachProc[i-1];
-        dataForEachProc[i] = restCols  / (numProcs - i);
-        shiftForEachProc[i] = shiftForEachProc[i-1] + dataForEachProc[i-1];
-    }
-
-    double *blockData = (double*)malloc(sizeof(double) * dataForEachProc[rank] * n2);
-
-    MPI_Scatterv(B, dataForEachProc, shiftForEachProc, coltype, blockData, dataForEachProc[rank] * n2,
-                 MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
-    return blockData;
-}
-
-
-double* dataDistributionA(double *A, int *dataForEachProc, int *shiftForEachProc, int numProcs, int rank){
-    dataForEachProc[0] = (n1 / numProcs) * n2;
-    shiftForEachProc[0] = 0;
-    int restRows = n1;
-    for (int i = 1; i < numProcs; ++i){
-        restRows -= dataForEachProc[i-1] / n2;
-        dataForEachProc[i] = restRows * n2 / (numProcs - i);
-        shiftForEachProc[i] = shiftForEachProc[i-1] + dataForEachProc[i-1];
-    }
-    double *blockData = (double*)malloc(sizeof(double) * dataForEachProc[rank]);
-    MPI_Scatterv(A, dataForEachProc, shiftForEachProc, MPI_DOUBLE, blockData, dataForEachProc[rank],
-                 MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    return blockData;
-}
-
-
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
     double *A, *B, *C;
     double *blockDataA, *blockDataB, *blockDataC;
-    int *dataForEachProc, *shiftForEachProc;
-    int x_C, y_C;
+    MPI_Comm colCom, rowCom;
+    int coords[2];
+    int dims[2] = {0, 0};
     int size, rank;
+    int sizeX, sizeY;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    dataForEachProc = (int*)malloc(sizeof(int) * size);
-    shiftForEachProc = (int*)malloc(sizeof(int) * size);
-
-    if (rank == 0){
-        srand(time(NULL));
+    if (rank == 0) {
         A = initMatrix(n1, n2);
         B = initMatrix(n2, n3);
-        C = (double*)malloc(sizeof(double) * n1 * n3);
-    }
-
-    blockDataA = dataDistributionA(A, dataForEachProc, shiftForEachProc, size, rank);
-    x_C = dataForEachProc[rank] / n2;
-    if (rank == 0){
-        for (int i = 0; i < x_C; ++i){
-            for (int j = 0; j < n2; ++j){
-                printf("%lf ", blockDataA[i*n2 + j]);
-            } putchar('\n');
-        }putchar('\n');
-    }
-
-    blockDataB = dataDistributionB(B, dataForEachProc, shiftForEachProc, size, rank);
-    y_C = dataForEachProc[rank];
-
-    if (rank == 0) {
-        for (int i = 0; i < dataForEachProc[rank]; ++i) {
+        /*for (int i = 0; i < n1; ++i) {
             for (int j = 0; j < n2; ++j) {
-                printf("%lf ", blockDataB[i * n2 + j]);
-            } putchar('\n');
+                printf("%lf ", A[i * n2 + j]);
+            }
+            putchar('\n');
         }
         putchar('\n');
-        blockDataC = calculateMatrixC(x_C, y_C, blockDataA, blockDataB);
-       for (int i = 0; i < x_C; ++i){
-            for (int j = 0; j < y_C; ++j){
-                printf("%lf ", blockDataC[i*y_C + j]);
-            } putchar('\n');
+        for (int i = 0; i < n2; ++i) {
+            for (int j = 0; j < n3; ++j) {
+                printf("%lf ", B[i * n3 + j]);
+            }
+            putchar('\n');
         }
+        putchar('\n');*/
+        C = (double *) malloc(sizeof(double) * n1 * n3);
     }
+
+    createCommunicators(coords, dims, size, &colCom, &rowCom);
+    MPI_Comm_size(colCom, &sizeX);
+    MPI_Comm_size(rowCom, &sizeY);
+    blockDataA = (double*) malloc(sizeof(double) * n1 * n2 / sizeX);
+    blockDataB = (double*) malloc(sizeof(double) * n2 * n3 / sizeY);
+    blockDataC = (double*)malloc(sizeof(double) * n1 * n3 / (sizeX * sizeY));
+    dataDistributionOnNodes(A, B, blockDataA, blockDataB, rowCom, colCom, coords, sizeX, sizeY);
+    /*if (coords[0] == 0 && coords[1] == 1){
+       for (int i = 0; i < 16; ++i){
+           printf("%lf ", blockDataA[i]);
+       } putchar('\n');
+       for (int i = 0; i < 16; ++i){
+           printf("%lf ", blockDataB[i]);
+       }
+    }*/
+
 
     MPI_Finalize();
     if (rank == 0){
         free(A);
         free(B);
         free(C);
-        free(blockDataC);
     }
-
     free(blockDataA);
     free(blockDataB);
-    free(dataForEachProc);
-    free(shiftForEachProc);
+    free(blockDataC);
     return 0;
 }
