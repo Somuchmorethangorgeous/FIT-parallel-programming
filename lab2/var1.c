93b0a9f603275b2ccf1b2a668adba4031e67cae3
Ruslan Popov <ruslanchik.popov@gmail.com>
2021-03-08 22:08:07 +0700

    Added directives for all loops

--- lab2/var1.c
+++ lab2/var1.c
@@ -6,11 +6,11 @@
 
 #define PI 3.14159265358979323846
 
-const int M_SIZE = 5;
+const int M_SIZE = 250;
 
 double norm(const double *v){
     double tmp = 0.0;
-#pragma parallel for num_threads(4)
+#pragma omp parallel for num_threads(4) reduction(+:tmp)
     for (int i = 0; i < M_SIZE; ++i) {
         tmp += v[i] * v[i];
     }
@@ -21,10 +21,10 @@ double norm(const double *v){
 bool answerIsGot(const double *A, const double *b, const double *x, const double normB){
     const double e = pow(10, -6);
     double *sol = (double*)malloc(sizeof(double) * M_SIZE);
-    // loop
+#pragma omp parallel num_threads(4)
     for (int i = 0; i < M_SIZE; ++i) {
-        double value = 0;
-        // loop
+        double value = 0.0;
+#pragma omp parallel for num_threads(4) reduction(+:value)
         for (int j = 0; j < M_SIZE; ++j) {
             value += A[i * M_SIZE + j] * x[j];
         }
@@ -38,12 +38,12 @@ bool answerIsGot(const double *A, const double *b, const double *x, const double
 
 void simpleIterationMethod(const double *A, const double *b, double *x){
     const double t = 0.01;
-    // loop
-    for (int i = 0; i < M_SIZE; ++i){
+#pragma omp parallel for num_threads(4)
+    for (int i = 0; i < M_SIZE; ++i) {
         double value = 0.0;
-        // loop
-        for (int j = 0; j < M_SIZE; ++j){
-            value += A[i*M_SIZE + j] * x[j];
+#pragma omp parallel for num_threads(4) reduction(+:value)
+        for (int j = 0; j < M_SIZE; ++j) {
+            value += A[i * M_SIZE + j] * x[j];
         }
         x[i] -= t * (value - b[i]);
     }
@@ -52,39 +52,40 @@ void simpleIterationMethod(const double *A, const double *b, double *x){
 
 double* solution(double *A, double *b, const double normB){
     double *x = (double*)malloc(sizeof(double) * M_SIZE);
-    // loop
+#pragma omp parallel for num_threads(4)
     for (int i = 0; i < M_SIZE; ++i){
-        x[i] = 0;
+        x[i] = 0.0;
     }
-    bool isEnd = false;
-    // loop?
+    bool isFinish = false;
     do {
         simpleIterationMethod(A, b, x);
-        isEnd = answerIsGot(A,b,x, normB);
-    } while (!isEnd);
+        isFinish = answerIsGot(A,b,x, normB);
+    } while (!isFinish);
     return x;
 }
 
 
 void initMatrixAndB(double *A, double *b){
     double *u = (double*)malloc(sizeof(double) * M_SIZE);
-#pragma omp parallel num_threads(4)
-    {
-        int i, j;
-        #pragma omp for
-        for (i = 0; i < M_SIZE; ++i) {
-            for (j = 0; j < M_SIZE; ++j) {
-                A[i * M_SIZE + j] = (i == j) ? 2.0 : 1.0;
-            }
+
+#pragma omp parallel for num_threads(4) collapse(2)
+    for (int i = 0; i < M_SIZE; ++i) {
+        for (int j = 0; j < M_SIZE; ++j) {
+            A[i * M_SIZE + j] = (i == j) ? 2.0 : 1.0;
         }
-        for (i = 0; i < M_SIZE; ++i) {
-            u[i] = sin((2 * PI * i) / M_SIZE);
-        }
-        for (i = 0; i < M_SIZE; ++i) {
-            b[i] = 0;
-            for (j = 0; j < M_SIZE; ++j) {
-                b[i] += A[i * M_SIZE + j] * u[j];
-            }
+    }
+
+#pragma omp parallel for num_threads(4)
+    for (int i = 0; i < M_SIZE; ++i) {
+        u[i] = sin((2 * PI * i) / M_SIZE);
+    }
+
+#pragma omp parallel for num_threads(4)
+    for (int i = 0; i < M_SIZE; ++i) {
+        b[i] = 0.0;
+#pragma omp parallel for num_threads(4) reduction(+:b[i])
+        for (int j = 0; j < M_SIZE; ++j) {
+            b[i] += A[i * M_SIZE + j] * u[j];
         }
     }
 #ifdef DEBUG_INFO
@@ -102,21 +103,21 @@ int main() {
     b = (double*)malloc(sizeof(double) * M_SIZE);
     initMatrixAndB(A,b);
     const double normB = norm(b);
-   //x = solution(A,b, normB);
+    x = solution(A,b, normB);
 
 #ifdef DEBUG_INFO
     /*for (int i = 0; i < M_SIZE; ++i){
         for (int j = 0; j < M_SIZE; ++j){
             printf("%lf ", A[i*M_SIZE+j]);
         } putchar('\n');
-    }
+    }*/
     printf("Answer is: ");
     for (int i = 0; i < M_SIZE; ++i){
         printf("%lf ", x[i]);
-    } putchar('\n');*/
+    } putchar('\n');
 #endif
     free(A);
     free(b);
-    //free(x);
+    free(x);
     return 0;
 }
