94b93085e8e011838100a956ec5abd1b5d055899
Ruslan Popov <ruslanchik.popov@gmail.com>
2021-05-19 17:10:28 +0700

    Added simple task stealing algorithm with thread.

--- lab5/main.cpp
+++ lab5/main.cpp
@@ -4,6 +4,10 @@
 #include <vector>
 
 #define TOTAL_NUMBER_ITER_PER_PROC 4
+#define TASKS_LEFT_TO_DO 5
+#define REQUEST_TAG 333
+#define RESPONSE_TAG 33
+#define SEND_TASK_TAG 3
 
 
 int size, rank;
@@ -44,10 +48,12 @@ void doWork(Task task){
 
 
 void* count(void* data){
+    double start, end;
     for(globalIter = 0; globalIter < TOTAL_NUMBER_ITER_PER_PROC; ++globalIter, curTask = 0){
         pthread_mutex_lock(&taskMutex);
         initTaskList();
 
+        start = MPI_Wtime();
         for(;curTask < taskList.size();++curTask){
             pthread_mutex_unlock(&taskMutex);
             doWork(taskList[curTask]);
@@ -55,16 +61,45 @@ void* count(void* data){
         }
 
         pthread_mutex_unlock(&taskMutex);
+        end = MPI_Wtime();
+        std::cout << "proc: " << rank << " time: "  << end - start << " at iter: " << globalIter << " tasks implemented: " << curTask << std::endl;
+
+        if (rank == 0){
+            std::cout << std::endl;
+        }
 
         MPI_Barrier(MPI_COMM_WORLD);
     }
     return nullptr;
 }
 
+void* unloadOfTasks(void* data){
+    int availabilityFlag;
+    MPI_Status status;
+    while (globalIter < TOTAL_NUMBER_ITER_PER_PROC){
+
+        pthread_mutex_lock(&taskMutex);
+        if(curTask > taskList.size() - TASKS_LEFT_TO_DO){
+            availabilityFlag = 0;
+        }
+        pthread_mutex_unlock(&taskMutex);
+
+        MPI_Send(&availabilityFlag, 1, MPI_INT, status.MPI_SOURCE, RESPONSE_TAG, MPI_COMM_WORLD);
+        if(!availabilityFlag){
+            continue;
+        }
+
+        Task taskToSend = taskList.back();
+        taskList.pop_back();
+        MPI_Send(&taskToSend, sizeof(Task), MPI_BYTE, status.MPI_SOURCE, SEND_TASK_TAG, MPI_COMM_WORLD);
+    }
+    return nullptr;
+}
 
 void initThreads(){
     pthread_attr_t attrs;
     pthread_t executorThread;
+    pthread_t receiverThread;
 
     if(pthread_attr_init(&attrs)){
         std::cerr << "Cannot initialize attributes" << std::endl;
@@ -78,12 +113,22 @@ void initThreads(){
 
     pthread_mutex_init(&taskMutex, nullptr);
 
+    if(pthread_create(&receiverThread, nullptr, unloadOfTasks, nullptr)){
+        std::cerr << "Error in creating thread" << std::endl;
+        errorInProgramm();
+    }
+
     if(pthread_create(&executorThread, nullptr, count, nullptr)){
         std::cerr << "Error in creating thread" << std::endl;
         errorInProgramm();
     }
 
     if(pthread_join(executorThread, nullptr)){
+        std::cerr << "Cannot join a thread!" << std::endl;
+        errorInProgramm();
+    }
+
+    if(pthread_join(receiverThread, nullptr)){
         std::cerr << "Cannot join a thread!" << std::endl;
         errorInProgramm();
     }
